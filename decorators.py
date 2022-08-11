2f7df0e01177fb54304299db0748511a1f7b26ba
mmmmyeaaah <mmmmyeaaah@yandex.ru>
2022-08-11 19:15:48 +0400

    first

--- decorators.py
+++ decorators.py
@@ -1,17 +1,18 @@
 from datetime import datetime
 
-def decorate(func, path=input('Введите имя папки, в которую сохранить логи: ')):
+def decorate(path=input('Введите имя папки, в которую сохранить логи: ')):
 
-    def _decorate():
-        def log():
-            res = func()
-            time = datetime.now()
-            name = func.__name__
-            arguments = func.__code__.co_varnames
-            my_list = [time, name, arguments, res]
-            with open(path + '/log_file.log', 'a') as log_file:
-                for arg in my_list:
-                    log_file.write(str(arg) + '\n')
+    def _decorate(func):
 
-        return log()  
-    return _decorate()
+        def log(*args, **kwargs):
+            with open(path + '/log_file.log', 'a', encoding='UTF-8') as log_file:
+                res = func(*args, **kwargs)
+                log_file.write(f'Имя функции: {func.__name__}' + '\n')
+                log_file.write(f'Время вызова: {datetime.now()}' + '\n')
+                log_file.write(f'Аргументы функции: {args}' + '\n')
+                log_file.write(f'Вернула значение: {res}' + '\n')
+            return res
+
+        return log
+
+    return _decorate
